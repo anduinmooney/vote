c0c7ab122bc5080baa10547493ce30fb163d5e3f
Anduin Mooney and James Donlan <anduin@>
2017-11-30 08:32:13 -0800

    initial commit, started scripting for age prompt

---
+++ js/scripts.js
@@ -0,0 +1,15 @@
+$(document).ready(function(){
+
+  var age = parseInt(prompt("How old are you?"));
+
+  if (age > 18 {
+    $('#vote').show();
+  } else {
+    $('#under-18').show();
+  }
+
+  var over18 = true;
+  if (over18) {
+    $('vote').show();
+  }
+});
